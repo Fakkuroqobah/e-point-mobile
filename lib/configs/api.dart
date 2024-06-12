89dc9885f56e051bbb53ef9291f97ddb1c12eee0
Fakkuroqobah <maulanaagung169@gmail.com>
2024-06-13 06:48:32 +0700

    update

--- lib/configs/api.dart
+++ lib/configs/api.dart
@@ -2,8 +2,7 @@ class Api {
   static const String baseUrl1 = "poinpelanggaransiswa.cloud";
   static const String baseUrl = "https://poinpelanggaransiswa.cloud/api/";
 
-  static const String downloadSurat =
-      "point-sekolah/surat_pemanggilan/download/";
+  static const String downloadSurat = "surat_pemanggilan/download/";
   static const String loginSiswa = "${baseUrl}login_siswa";
   static const String loginOrtu = "${baseUrl}login_ortu";
   static const String jumlahPoint = "${baseUrl}jumlah_point?id=";
